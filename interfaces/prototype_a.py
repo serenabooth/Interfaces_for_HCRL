ac3304a5b34f909381dd474822a79cb29ea8a417
serena booth <sbooth@mit.edu>
2020-04-03 08:09:41 -0400

    interface in progress

--- interfaces/prototype_a.py
+++ interfaces/prototype_a.py
@@ -11,10 +11,12 @@ class Interface():
 
     GUI = None
     img_array = []
+    button_array = []
 
-    def callback(self, id, feedback):
+    def callback(self, id, feedback, feedback_target=None):
         print (id)
         print (feedback)
+        print (feedback_target)
 
     def add_button(self, canvas, x, good_button, bad_button, button_id):
         bad_robot_button = tk.Button(self.GUI, image=bad_button, command=lambda: self.callback(button_id, "bad"))
@@ -24,14 +26,18 @@ class Interface():
         good_robot_button.configure(background="white")
         good_robot_button.place(x=x + 150, y=170)
 
+        return (bad_robot_button, good_robot_button)
+
 
     def add_image(self, canvas, file, x):
         img = tk.PhotoImage(file=file)
         img = img.zoom(2) #with 250, I ended up running out of memory
         img = img.subsample(5) #mechanically, here it is adjusted to 32 in
         canvas.create_image(x + img.width()/2, 75, image=img)
-        self.img_array.append(img)
+        return img
 
+    def update_image(self, file, img):
+        todo
 
     def run(self):
         self.GUI = tk.Tk()
@@ -48,18 +54,42 @@ class Interface():
         img_good_robot_btn = ImageTk.PhotoImage(good_button_image)
 
 
-        self.add_button(canvas, x=20, good_button=img_good_robot_btn, bad_button=img_bad_robot_btn, button_id = 1)
-        self.add_button(canvas, x=20+290, good_button=img_good_robot_btn, bad_button=img_bad_robot_btn, button_id = 2)
-        self.add_button(canvas, x=20+290*2, good_button=img_good_robot_btn, bad_button=img_bad_robot_btn, button_id=3)
-        self.add_button(canvas, x=20+290*3, good_button=img_good_robot_btn, bad_button=img_bad_robot_btn, button_id=4)
-        self.add_button(canvas, x=20+290*4, good_button=img_good_robot_btn, bad_button=img_bad_robot_btn, button_id=5)
+        for i in range(1,5):
+            (bad,good) = self.add_button(canvas, x=20+290*i, good_button=img_good_robot_btn, bad_button=img_bad_robot_btn, button_id = i)
+            self.button_array.append((bad,good))
+            img = self.add_image(canvas, "Screenshots/tmp/" + str(i) + ".png", x=20+290*i)
 
+
+
+        print (self.button_array)
+        print (len(self.button_array))
 
         count = 1
         while 1:
 
             img1 = tk.PhotoImage(file="Screenshots/tmp/" + str(count + 5) + ".png")
             canvas.create_image(WIDTH/2, HEIGHT-int(img1.height()/2), image=img1)
+
+            # there is some strange bug when putting this in a loop...
+            # all button_ids become the same
+            callback_array = []
+            for i in range(0,4):
+                callback_array.append(self.callback)
+            print (callback_array)
+            # for i in range(0, 4):
+            #     self.button_array[i][0].configure(command=lambda: self.callback(str(i)+"", "bad", count))
+            #     self.button_array[i][1].configure(command=lambda: self.callback(str(i)+"", "good", count))
+            #
+            # print (self.button_array)
+            # self.button_array[0][0].configure(command=lambda: self.callback(str(0), "bad", count))
+            # self.button_array[0][1].configure(command=lambda: self.callback(str(0), "good", count))
+            # self.button_array[1][0].configure(command=lambda: self.callback(str(1), "bad", count))
+            # self.button_array[1][1].configure(command=lambda: self.callback(str(1), "good", count))
+            # self.button_array[2][0].configure(command=lambda: self.callback(str(2), "bad", count))
+            # self.button_array[2][1].configure(command=lambda: self.callback(str(2), "good", count))
+            # self.button_array[3][0].configure(command=lambda: self.callback(str(3), "bad", count))
+            # self.button_array[3][1].configure(command=lambda: self.callback(str(3), "good", count))
+
 
             self.GUI.update()
             time.sleep(0.1)
