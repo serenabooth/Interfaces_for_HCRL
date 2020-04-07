a547fd6caf45756f330c3c4a6b7300cd32c0f0e5
serena booth <sbooth@mit.edu>
2020-04-07 11:54:11 -0400

    about to simplify coach

--- RL_Methods/COACH.py
+++ RL_Methods/COACH.py
@@ -7,41 +7,101 @@ import datetime, time
 
 from multiprocessing import Queue
 from copy import deepcopy
+from collections import namedtuple
+
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        # actor's layer
+        self.action_head = nn.Linear(4, 2)
+        self.saved_actions = []
+
+    def forward(self, x):
+        # actor: choses action to take from state s_t
+        # by returning probability of each action
+        action_prob = F.softmax(self.action_head(x), dim=-1)
+
+        # return values for both actor and critic as a tuple of 2 values:
+        # 1. a list with the probability of each action over the action space
+        # 2. the value from state s_t
+        return action_prob
 
 
+model = Policy() # init policy
+optimizer = optim.SGD(model.parameters(), lr=5e-3)
 
-def COACH(domain, trace_values = [0.95], delay = 6, learning_rate = 0.95):
+
+def select_action(state):
+    state = torch.from_numpy(state).float()
+    action_probs = model(state)
+
+    action = np.argmax(action_probs.detach().numpy())
+
+    # TODO not sure I need to make this categorical
+    m = Categorical(action_probs)
+    print (m.log_prob(torch.tensor(action)))
+    model.saved_actions.append(m.log_prob(torch.tensor(action)))
+
+    # the action to take (left or right)
+    return (action, action_probs)
+
+def COACH(domain,
+               trace_set = [0.95],
+               delay = 6,
+               learning_rate = 2.5e-4):
+
+    prob_histories = []
     state = domain.get_current_features()
-
-    policy_weights = torch.zeros(len(state), requires_grad=True)
-
-    print (policy_weights.grad)
-    actions = domain.get_possible_actions()
-
-    traces = {}
-    for trace_val in trace_values:
-        traces[trace_val] = torch.zeros(len(state))
-
-    print (traces)
+    eligibility_traces = {}
+    for trace_value in trace_set:
+        eligibility_traces[trace_value] = torch.zeros(2, requires_grad=False)
+        print (eligibility_traces[trace_value])
+    first_episode = True
 
     while True:
-        print ('executing')
-        action = 0 if np.dot(policy_weights.detach(), state) < 0 else 1
-        env_reward, done = domain.take_action(action)
+        action, action_probs = select_action(state)
+        reward, done = domain.take_action(action)
+        new_state = domain.get_current_features()
+        prob_histories.append((state, action, reward, action_probs))
 
-        state = domain.get_current_features()
-        human_feedback = 1
+        advantage = reward
+        loss = -1 * model.saved_actions[-1] * advantage
+        model.zero_grad()
+        loss.backward()
 
-        if policy_weights.grad != None:
-            traces[0.95] = 0.95 * traces[0.95] + policy_weights.grad
+        print ("success")
+        print (  1 / prob_histories[-1][3] )
 
-        print (action)
-        # exit()
-        policy_weights = policy_weights + learning_rate * human_feedback  
+        for trace_gamma in trace_set:
+            for p in model.parameters():
+                eligibility_traces[trace_gamma] = trace_gamma * eligibility_traces[trace_gamma] + 1 / prob_histories[-1][3]
+
+        print ('made it here')
+        exit()
+
+        for p in model.parameters():
+            print (p.grad)
+        with torch.no_grad():
+
+            for p in model.parameters():
+                new_val = p.grad * 0.1
+                p.copy_(new_val)
+                print (p.grad)
+
+        print ("success 2")
+
+        # print(eligibility_traces)
 
 
 
+
+        state = new_state
 if __name__ == "__main__":
     domain = CartPole()
 
