49b34391a458af7628da5b9bba8cb4ea21952ac8
serena booth <sbooth@mit.edu>
2020-04-08 13:01:04 -0400

    some coach progress, but may reattempt

--- RL_Methods/COACH.py
+++ RL_Methods/COACH.py
@@ -1,6 +1,9 @@
 import sys
 sys.path.append("../Domains")
 from cartpole import CartPole
+from gridworld import Gridworld
+from torchvision import transforms
+
 import cartpole_oracle
 
 import numpy as np
@@ -11,7 +14,7 @@ from copy import deepcopy
 import random
 import torch
 
-def COACH(domain, weights, trace_set = [0.95, 0.35], delay = 6, learning_rate = 1e-3, oracle_parameters = None):
+def COACH(domain, weights, trace_set = [0.95], delay = 6, learning_rate = 5e-2, oracle_parameters = None):
     state = domain.reset()
     grads = []
     total_reward = 0
@@ -20,59 +23,72 @@ def COACH(domain, weights, trace_set = [0.95, 0.35], delay = 6, learning_rate = 
 
     eligibility_traces = {}
     for trace_value in trace_set:
-        eligibility_traces[trace_value] = torch.zeros(2, requires_grad=False)
+        eligibility_traces[trace_value] = torch.zeros(4, requires_grad=True)
 
-    while not done:
+    for _ in range(0, 1000):
+
         human_reward = 0
         state = torch.from_numpy(state).float()
+        # print ("Weights: " + str(weights))
+        # print (state)
         z = torch.matmul(state, weights)
+        # print (z)
         probs = torch.nn.Softmax(dim=0)(z)
-        print (probs)
-        action = np.argmax(probs.detach().numpy())
+        print ("Probs " + str(probs))
+        # if random.random() < 0.2:
+        #     action = random.randint(0,3)
+        # else:
+        #     action = np.argmax(probs.detach().numpy())
+        # print (probs)
+        m = torch.distributions.Categorical(probs)
+        action = m.sample()
+        # print (action)
+
         #action = int(torch.bernoulli(max(probs[1])).item())
 
         d_softmax = torch.diag(probs) - probs.view(-1, 1) * probs
+
         d_log = d_softmax[action] / probs[action]
         grad = state.view(-1, 1) * d_log
+        print ("Gradient: " + str(grad))
         grads.append(grad)
 
-
-        if random.random() < 0.2:
+        if oracle_parameters != None and random.random() < 0.2:
             human_reward = cartpole_oracle.ask_oracle_advice(domain, oracle_parameters, action)
             print ('human reward' + str(human_reward))
 
-
         reward, done = domain.take_action(action)
-        state = domain.last_observation
-
-
+        state = np.array(domain.last_observation)
+        if done:
+            break
+        human_reward = reward
 
         total_reward += reward
-        num_steps += 1
-
         # for num_steps, grad in enumerate(grads):
-        if human_reward > 0:
-            for trace_value in trace_set:
-                eligibility_traces[trace_value] = trace_value * eligibility_traces[trace_value] + 1 / probs * grad
+        # if human_reward > 0:
+        # for trace_value in trace_set:
+        #     eligibility_traces[trace_value] = trace_value * eligibility_traces[trace_value] + 1 / probs * grad
 
-            weights += learning_rate * human_reward * eligibility_traces[0.95]
+        weights += learning_rate * human_reward * grad / probs
 
     return (total_reward, grads)
     # todo
 
 if __name__ == "__main__":
-    domain = CartPole()
+    domain = Gridworld()
 
-    print ("training oracle")
-    oracle_parameters = cartpole_oracle.train(domain)
-    print ("oracle trained")
+    # print ("training oracle")
+    # oracle_parameters = cartpole_oracle.train(domain)
+    # print ("oracle trained")
 
     n_state = domain.obs_size
     n_action = domain.num_actions
-    weights = torch.rand(n_state, n_action)
+    print (n_action)
+    weights = torch.zeros(n_state, n_action)
+    print (weights)
 
-    for i in range(0, 1000):
-        total_reward, grads = COACH(domain, weights, oracle_parameters=oracle_parameters)
-        print (total_reward)
+    for i in range(0, 10):
+        total_reward, grads = COACH(domain, weights)
+        print ("total reward " + str(total_reward))
         # for j, grad in enumerate(grads):
         #     weights += 0.001 * grad * (total_reward - j)
