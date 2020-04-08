e6808729eade703c208aeace452142ad0d446dcf
serena booth <sbooth@mit.edu>
2020-04-08 14:44:47 -0400

    coach still broken

--- RL_Methods/COACH.py
+++ RL_Methods/COACH.py
@@ -14,7 +14,7 @@ from copy import deepcopy
 import random
 import torch
 
-def COACH(domain, weights, trace_set = [0.95], delay = 6, learning_rate = 5e-2, oracle_parameters = None):
+def COACH(domain, weights, trace_set = [0], delay = 6, learning_rate = 5e-4, oracle_parameters = None):
     state = domain.reset()
     grads = []
     total_reward = 0
@@ -23,59 +23,46 @@ def COACH(domain, weights, trace_set = [0.95], delay = 6, learning_rate = 5e-2, 
 
     eligibility_traces = {}
     for trace_value in trace_set:
-        eligibility_traces[trace_value] = torch.zeros(4, requires_grad=True)
+        eligibility_traces[trace_value] = torch.zeros(domain.num_actions, requires_grad=True)
 
     for _ in range(0, 1000):
 
         human_reward = 0
         state = torch.from_numpy(state).float()
-        # print ("Weights: " + str(weights))
-        # print (state)
         z = torch.matmul(state, weights)
-        # print (z)
         probs = torch.nn.Softmax(dim=0)(z)
-        print ("Probs " + str(probs))
-        # if random.random() < 0.2:
-        #     action = random.randint(0,3)
-        # else:
-        #     action = np.argmax(probs.detach().numpy())
-        # print (probs)
-        m = torch.distributions.Categorical(probs)
-        action = m.sample()
-        # print (action)
+        action = np.argmax(probs.detach().numpy())
+        # m = torch.distributions.Categorical(probs)
+        # action = m.sample().item()
 
-        #action = int(torch.bernoulli(max(probs[1])).item())
-
+        # compute gradient
         d_softmax = torch.diag(probs) - probs.view(-1, 1) * probs
-
         d_log = d_softmax[action] / probs[action]
         grad = state.view(-1, 1) * d_log
-        print ("Gradient: " + str(grad))
         grads.append(grad)
 
-        if oracle_parameters != None and random.random() < 0.2:
-            human_reward = cartpole_oracle.ask_oracle_advice(domain, oracle_parameters, action)
-            print ('human reward' + str(human_reward))
-
         reward, done = domain.take_action(action)
+        total_reward += reward
         state = np.array(domain.last_observation)
         if done:
             break
-        human_reward = reward
 
-        total_reward += reward
-        # for num_steps, grad in enumerate(grads):
-        # if human_reward > 0:
-        # for trace_value in trace_set:
-        #     eligibility_traces[trace_value] = trace_value * eligibility_traces[trace_value] + 1 / probs * grad
+        if oracle_parameters != None:
+            human_reward = cartpole_oracle.ask_oracle_advice(domain, oracle_parameters, action)
+        else:
+            human_reward = total_reward
 
-        weights += learning_rate * human_reward * grad / probs
+
+        for trace_value in trace_set:
+            eligibility_traces[trace_value] = trace_value * eligibility_traces[trace_value] + (1 / probs[action]) * grad[action]
+
+        weights += learning_rate * human_reward * eligibility_traces[0]
 
     return (total_reward, grads)
     # todo
 
 if __name__ == "__main__":
-    domain = Gridworld()
+    domain = CartPole()
 
     # print ("training oracle")
     # oracle_parameters = cartpole_oracle.train(domain)
@@ -83,11 +70,9 @@ if __name__ == "__main__":
 
     n_state = domain.obs_size
     n_action = domain.num_actions
-    print (n_action)
     weights = torch.zeros(n_state, n_action)
-    print (weights)
 
-    for i in range(0, 10):
+    for i in range(0, 1000):
         total_reward, grads = COACH(domain, weights)
         print ("total reward " + str(total_reward))
         # for j, grad in enumerate(grads):
