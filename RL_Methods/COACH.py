e7660a38d96b7f3c535667ec52f07fdc1b2b5839
serena booth <sbooth@mit.edu>
2020-04-13 17:50:52 -0400

    COACH reimplementation working!

--- RL_Methods/COACH.py
+++ RL_Methods/COACH.py
@@ -12,6 +12,7 @@ from copy import deepcopy
 
 import random
 import torch
+import matplotlib.pyplot as plt
 
 def randargmax(b,**kw):
   """ a random tie-breaking argmax"""
@@ -67,7 +68,7 @@ def rargmax(vector):
     print ("Vector " + str(vector) + ", had to randomly pick")
     return random.choice(range(4))
 
-def COACH(domain, theta, learning_rate = 0.05, oracle_parameters = None):
+def COACH(domain, theta, learning_rate = 0.05, oracle_parameters = None, reward_fn = 0):
     state = domain.reset()[:]
     grads = []
     total_reward = 0
@@ -75,31 +76,23 @@ def COACH(domain, theta, learning_rate = 0.05, oracle_parameters = None):
     num_steps = 0
     action_history = []
 
-    for k in range(0,200):
+    for k in range(0, 1000):
         num_steps += 1
 
         human_reward = 0
 
-        # z = weights[state[0]][state[1]] # get weights for each action
-
-        pi = torch.nn.Softmax(dim=1)(torch.tensor(theta))
+        pi = torch.nn.Softmax(dim=-2)(torch.tensor(theta))
 
         action_weights = pi[state[0] * 5 + state[1]]
-        # print (action_weights)
-        # p = action_weights / action_weights.sum()
-        # print (p)
-        # stochastic action selection
 
+        if random.random() < 0.8:
+            # m = torch.distributions.Categorical(action_weights)
+            # action = m.sample().item()
+            action = rargmax(action_weights.numpy())
 
-        # action = np.argmax(probs) #m.sample().item()
-        # action = np.random.choice(range(len(action_weights)), p=p)
-
-        if random.random() < 0.9:
-            m = torch.distributions.Categorical(action_weights)
-            action = m.sample().item()
         else:
             action = random.randint(0, 3)
-            # action = rargmax(action_weights.numpy())
+        # action = rargmax(action_weights.numpy())
 
         #
         # print ("State " + str(state))
@@ -113,24 +106,19 @@ def COACH(domain, theta, learning_rate = 0.05, oracle_parameters = None):
 
         grads.append(jacobian)
 
-        reward, done = domain.take_action(action)
-        total_reward += reward
-        human_reward = total_reward
+        reward, done = domain.take_action(action, reward_fn = reward_fn)
+        human_reward = reward
         if done:
             break
-        theta += learning_rate * jacobian[state[0] * 5 + state[1]][action].numpy() * human_reward / action_weights[action].numpy()
+        theta_delta = learning_rate * jacobian[state[0] * 5 + state[1]][action].numpy() * human_reward / action_weights[action].numpy()
+        theta = theta + theta_delta
 
         state = np.array(domain.position)[:]
 
+        total_reward += reward
 
-    # print (theta)
-    print (action_history[0:10])
-    print (num_steps)
-    # if done:
-    #     print ("success!")
-    #     print (total_reward)
-    #     exit()
-    return (total_reward, grads, done, theta)
+
+    return (total_reward, done, theta, num_steps)
     # todo
 
 
@@ -146,8 +134,23 @@ if __name__ == "__main__":
 
     theta = np.zeros((n_state, n_action))
 
+    total_num_steps_0 = []
+
+    total_num_steps_1 = []
+
     for i in range(0, 100):
-        total_reward, grads, done, theta = COACH(domain, theta)
+        total_reward, done, theta, num_steps = COACH(domain, theta, reward_fn = 0)
+        total_num_steps_0.append(num_steps)
+        print ("total reward " + str(total_reward) + ", done: " + str(done))
+
+    for i in range(0, 100):
+        total_reward, done, theta, num_steps = COACH(domain, theta, reward_fn = 1)
+        total_num_steps_1.append(num_steps)
         print ("total reward " + str(total_reward) + ", done: " + str(done))
         # for j, grad in enumerate(grads):
         #     weights += 0.001 * grad * (total_reward - j)
+    plt.plot(np.log(total_num_steps_0), label = "task based")
+    plt.plot(np.log(total_num_steps_1), label = "policy independent")
+    plt.legend()
+    plt.ylim((0,8))
+    plt.show()
