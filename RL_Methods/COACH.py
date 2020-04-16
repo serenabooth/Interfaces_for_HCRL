fa3867f5b7f73a13c23f0589c1050e50343bdf8c
serena booth <sbooth@mit.edu>
2020-04-16 12:23:53 -0400

    coach, bugfix and cleaned up

--- RL_Methods/COACH.py
+++ RL_Methods/COACH.py
@@ -14,14 +14,13 @@ import random
 import torch
 import matplotlib.pyplot as plt
 
-def rand_argmax(b):
-  return np.argmax(np.random.random(b.shape) * (b==b.max()))
-
+def randargmax(b,**kw):
+  """ a random tie-breaking argmax """
+  return np.argmax(np.random.random(b.shape) * (b==b.max()), **kw)
 
 def softmax_grad(s):
     """
     Compute the gradient of a softmax function.
-
     Params
     ------
         s : a numpy matrix
@@ -30,13 +29,35 @@ def softmax_grad(s):
     jacobian = np.diagflat(s) - np.dot(SM, SM.T)
     return jacobian
 
-def COACH(domain,
-            theta,
-            num_episodes = 100,
-            trace_set = [0.9],
-            learning_rate = 0.05,
-            delay = 0,
-            reward_fn = 0):
+def evaluate_COACH(domain, theta, reward_fn = 0):
+    average_num_steps = []
+    for _ in range(0, 5):
+        state = domain.reset()[:]
+
+        for num_steps in range(0, 2000):
+
+            pi = torch.nn.Softmax(dim=-1)(torch.tensor(theta))
+            action_weights = pi[state[0]][state[1]]
+
+            if random.random() < 0.8:
+                action = randargmax(action_weights.numpy())
+            else:
+                action = random.randint(0, domain.num_actions-1)
+
+
+            # compute gradient
+            jacobian = softmax_grad(pi)
+            jacobian = jacobian.reshape(40,4,40,4)
+
+            reward, done = domain.take_action(action, reward_fn = reward_fn)
+            state = np.array(domain.position)[:]
+
+            if done:
+                break
+        average_num_steps.append(num_steps)
+    return average_num_steps
+
+def COACH(domain, num_episodes = 100, trace_set = [0], delay = 0, learning_rate = 0.05, reward_fn = 0):
     """
     Implements COACH, a method for human-centered RL
 
@@ -45,17 +66,16 @@ def COACH(domain,
 
     Params
     ------
-        theta : np.array()
-            Contains the weights for decision-making.
         num_episodes : int
             The number of episodes to train for
         trace_set : list
             A list containing all eligibility trace values - e.g. [0.3, 0.9]
+        delay : int
+            An int telling us how many timesteps human feedback is delayed
         learning_rate : float
             A hyperparameter which determines how rapidly to incorporate feedback
         reward_fn : int
             A parameter passed to the domain to specify which reward function to use
-
     Returns
     -------
         rewards_all_episodes: list of ints
@@ -63,84 +83,88 @@ def COACH(domain,
         num_steps_all_episodes: list of ints
             A list of step counts from each episode
     """
-    rewards_all_episodes = []
-    num_steps_all_episodes = []
-
-    # initialize traces to zeros
-    eligibility_traces = {}
-    for trace_val in trace_set:
-        eligibility_traces[trace_val] = np.zeros(theta.shape)
-
-    for _ in range(0, num_episodes):
-        state = domain.reset()[:]
-        total_reward = 0
-        done = False
-        num_steps = 0
-
-
-        # limit the number of episode steps
-        for _ in range(0, 2000):
-            num_steps += 1
-            human_reward = 0
-
-            pi = torch.nn.Softmax(dim=-2)(torch.tensor(theta))
-
-            action_weights = pi[state[0] * 5 + state[1]]
-
-            # Epsilon-greedy action selection
-            if random.random() < 0.8:
-                # Choose action with argmax. Break ties randomly.
-                action = rand_argmax(action_weights.numpy())
-
-            else:
-                action = random.randint(0, 3)
-
-            # compute gradient of policy w.r.t. theta
-            jacobian = softmax_grad(pi)
-            jacobian = jacobian.reshape(domain.num_states, domain.num_actions, domain.num_states, domain.num_actions)
-
-            reward, done = domain.take_action(action, reward_fn = reward_fn)
-            human_reward = reward
-            total_reward += reward
-
-            for trace_val in eligibility_traces.keys():
-                eligibility_traces[trace_val] = trace_val * eligibility_traces[trace_val]
-                eligibility_traces[trace_val] += 1.0 / action_weights[action].numpy() * jacobian[state[0] * 5 + state[1]][action].numpy()
-
-            if done:
-                print ("Successfully finished episode. Num steps " + str(num_steps))
-                break
-
-            trace_val = domain.select_trace(trace_set, human_reward)
-            theta += learning_rate * human_reward * eligibility_traces[trace_val]
-            state = np.array(domain.position)[:]
-
-        rewards_all_episodes.append(total_reward)
-        num_steps_all_episodes.append(num_steps)
-
-    return (rewards_all_episodes, num_steps_all_episodes)
-
-if __name__ == "__main__":
-    domain = Gridworld()
-
-    # print ("training oracle")
-    # oracle_parameters = cartpole_oracle.train(domain)
-    # print ("oracle trained")
 
     n_state = domain.num_states
     n_action = domain.num_actions
 
-    theta = np.zeros((n_state, n_action))
+    theta = np.zeros((n_state[0], n_state[1], n_action))
 
-    total_num_steps_0 = []
+    evaluated_episodes = []
+    total_steps = []
 
-    total_num_steps_1 = []
+    for episode_id in range(0, num_episodes):
+        state = domain.reset()[:]
+        episode_reward = 0
+        done = False
+        theta_delta = []
 
-    total_reward, total_num_steps_0 = COACH(domain, theta, num_episodes = 50, trace_set = [0], reward_fn = 0)
-    total_reward, total_num_steps_1 = COACH(domain, theta, num_episodes = 50, trace_set = [0], reward_fn = 1)
+        eligibility_traces = {}
+        for trace_val in trace_set:
+            eligibility_traces[trace_val] = np.zeros(theta.shape)
 
-    plt.plot(np.log(total_num_steps_0), label = "task based")
-    plt.plot(np.log(total_num_steps_1), label = "policy independent")
+        for num_steps in range(0, 2000):
+            human_reward = 0
+
+            # print(torch.tensor(theta).shape)
+            pi = torch.nn.Softmax(dim=-1)(torch.tensor(theta))
+
+            action_weights = pi[state[0]][state[1]]
+
+            if random.random() < 0.8:
+                action = randargmax(action_weights.numpy())
+            else:
+                action = random.randint(0, domain.num_actions-1)
+
+            # compute gradient
+            jacobian = softmax_grad(pi)
+            jacobian = jacobian.reshape(8,5,4,8,5,4)
+
+            reward, done = domain.take_action(action, reward_fn = reward_fn)
+            human_reward = reward
+            episode_reward += reward
+
+            selected_trace = domain.select_trace(trace_set, human_reward)
+
+            if done:
+                break
+
+            for trace_val in trace_set:
+                eligibility_traces[trace_val] = trace_val * eligibility_traces[trace_val]
+                eligibility_traces[trace_val] += 1.0 / action_weights[action].numpy() * jacobian[state[0]][state[1]][action].numpy()
+
+            theta_delta = learning_rate * human_reward * eligibility_traces[selected_trace]
+            theta = theta + theta_delta
+            state = np.array(domain.position)[:]
+
+        if episode_id % 3 == 0:
+            evaluated_episodes.append(episode_id)
+            evaluated_num_steps = evaluate_COACH(domain, theta)
+            total_steps.append(evaluated_num_steps)
+
+    return total_steps, evaluated_episodes
+    # todo
+
+def gridworld_test():
+    domain = Gridworld()
+
+    for reward_fn in [0,1]:
+
+        total_num_steps, eval_episodes = COACH(domain, trace_set = [0], reward_fn = reward_fn)
+        log_mean_steps = np.log(np.mean(total_num_steps, axis = 1))
+        log_num_steps_std = np.std(np.log(total_num_steps), axis=1)
+
+        label = ""
+        if reward_fn == 0:
+            label = "task based"
+        elif reward_fn == 1:
+            label = "policy indepdendent"
+        plt.plot(eval_episodes, log_mean_steps, label = label)
+        plt.fill_between(eval_episodes, log_mean_steps - log_num_steps_std,
+                                        log_mean_steps + log_num_steps_std, alpha=0.2)
+
     plt.legend()
     plt.ylim((0,8))
     plt.show()
+
+if __name__ == "__main__":
+    gridworld_test()
