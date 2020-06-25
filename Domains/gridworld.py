66ad600a3efe99ee1428c20703774c735498ee1e
serena booth <sbooth@mit.edu>
2020-06-25 11:22:17 -0400

    fixed old merge conflict in gridworld.py

--- Domains/gridworld.py
+++ Domains/gridworld.py
@@ -2,49 +2,11 @@ from domain import Domain
 from enum import Enum
 import numpy as np
 
-<<<<<<< HEAD
-class Gridworld(Domain):
-    env = None
-    last_observation = None
-    num_actions = None
-    obs_size = None
-    position = None
-    goal = None
-    def __init__(self):
-        # Use OpenAI Gym for CartPole environment
-        self.env = np.zeros(shape = (8,5))
-        print (self.env)
-        self.position = [0,0]
-        self.env[self.position[0]][self.position[1]] = 1
-        self.goal = [7,4]
-
-
-        self.env[self.goal[0]][self.goal[1]] = 2
-
-
-        self.last_observation = self.position
-        self.num_actions = 4
-        self.obs_size = 2
-
-    def reset(self):
-        self.position = [0,0]
-        self.env[self.position[0]][self.position[1]] = 1
-        self.goal = [7,4]
-        self.last_observation = self.position
-        return np.array(self.last_observation)
-
-    def get_state_vec(self):
-        return self.last_observation
-
-    def get_current_features(self):
-        return self.last_observation
-=======
 class Actions(Enum):
     UP = 0
     DOWN = 1
     LEFT = 2
     RIGHT = 3
-
 
 class RewardFunction(Enum):
     TASK_BASED = 0
@@ -72,50 +34,12 @@ class Gridworld(Domain):
 
     def get_current_features(self):
         return self.position
->>>>>>> a474e3b998b18c23ff1f009ec600a49d52029611
 
-    def get_future_features(self, state, action):
+    def get_future_features(self):
         pass
 
     def get_possible_actions(self, constraint=None):
-<<<<<<< HEAD
         return ["UP", "DOWN", "LEFT", "RIGHT"]
-
-    def take_action(self, action):
-        if action == 0:
-            action = "UP"
-        if action == 1:
-            action = "DOWN"
-        if action == 2:
-            action = "LEFT"
-        if action == 3:
-            action = "RIGHT"
-
-        done = False
-        self.env[self.position[0]][self.position[1]] = 0
-
-
-        if action == "DOWN" and self.position[0] < 7:
-            self.position[0] += 1
-        if action == "UP" and self.position[0] > 0:
-            self.position[0] -= 1
-        if action == "RIGHT" and self.position[1] < 4:
-            self.position[1] += 1
-        if action == "LEFT" and self.position[1] > 0:
-            self.position[1] -= 1
-
-        self.env[self.position[0]][self.position[1]] = 1
-        self.env[self.goal[0]][self.goal[1]] = 2
-
-        done = self.position == self.goal
-        if done:
-            reward = 5
-        elif self.position[0] >= 1 and self.position[0] <= 6:
-            reward = -1
-        else:
-            reward = 0
-=======
-        return len(Actions)
 
     def select_trace(self, trace_set, reward):
         """
@@ -184,7 +108,6 @@ class Gridworld(Domain):
             reward = self.task_based_reward(initial_position, done)
         elif reward_fn == RewardFunction.POLICY_INDEPENDENT:
             reward = self.policy_independent_reward(initial_position, done, action)
->>>>>>> a474e3b998b18c23ff1f009ec600a49d52029611
 
         return (reward, done)
 
@@ -192,7 +115,6 @@ class Gridworld(Domain):
 
 if __name__ == "__main__":
     gridworld = Gridworld()
-<<<<<<< HEAD
     print (gridworld.env)
 
     totalreward = 0
@@ -218,6 +140,3 @@ if __name__ == "__main__":
         # if done:
         #     break
     print (totalreward)
-=======
-    print (gridworld.position)
->>>>>>> a474e3b998b18c23ff1f009ec600a49d52029611
